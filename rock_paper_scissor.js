f391f32541d4b36f6cc4e49167ef0663f524e3eb
Jacob Adams big-boi <jacob.adams121202@gmail.com>
2024-04-15 21:32:28 -0700

    fixed playRound

--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -1,30 +1,29 @@
 function playRound(playerSelection, computerSelection) {
-  switch (playerSelection.toLowerCase()) {
-    case computerSelection.toLowerCase() === playerSelection.toLowerCase():
-      return null;
-      break;
-    case "rock":
-      if (computerSelection.toLowerCase() === "paper") {
-        return false;
-      }
-      return true;
-      break;
-    case "scissors":
-      if (computerSelection.toLowerCase() === "rock") {
-        return false;
-      }
-      return true;
-      break;
-    case "paper":
-      if (computerSelection.toLowerCase() === "scissors") {
-        return false;
-      }
-      return true;
-      break;
-    default:
-      console.log("Default case reached, ERROR");
-      break;
-  } 
+  function playRound(playerSelection, computerSelection) {
+    switch (playerSelection.toLowerCase()) {
+      case "rock":
+        if (computerSelection.toLowerCase() === "paper") {
+          return false;
+        }
+        break; // Add a break statement here
+      case "scissors":
+        if (computerSelection.toLowerCase() === "rock") {
+          return false;
+        }
+        break; // Add a break statement here
+      case "paper":
+        if (computerSelection.toLowerCase() === "scissors") {
+          return false;
+        }
+        break; // Add a break statement here
+      default:
+        if (playerSelection !== computerSelection) {
+          return true;
+        } else {
+          return null;
+        }
+    }
+  }
 }
 
 function getComputerChoice() {
@@ -34,11 +33,23 @@ function getComputerChoice() {
   return choices[numChoice];
 }
 
-const playerSelection = "rock";
-const computerSelection = "rock";
+function getUserChoice(){
+  let userInput = prompt("Please enter your rock, paper, or scissors: ")
+  if (userInput !== null) {
+    console.log("User entered: ", userInput)
+  } else {
+    console.log("User cancelled input")
+  }
 
+  return userInput;
+}
 
+let outcome = playRound(playerSelection, computerSelection);
 
-let outcome = null;
-console.log(playerSelection + " vs " + computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+if (outcome === false) {
+  console.log("You Lose! " + computerSelection + " beats " + playerSelection);
+} else if (outcome === true) {
+  console.log("You Win! " + playerSelection + " beats " + computerSelection);
+} else {
+  console.log("It was a tie, try again");
+}
